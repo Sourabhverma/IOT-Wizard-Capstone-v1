91acb10f30932477223fea2377932d436d9d4789
sourabhv@vmware.com <sourabhv@vmware.com>
2021-10-30 17:19:31 +0530

    push daya schema update 2

--- app/app.py
+++ app/app.py
@@ -24,22 +24,24 @@ def index():
     )
 
 @application.route('/bin-lat', methods=['GET'])
-def get_veh_dist_lat():
+def get_bin_lat_updated():
     output_cur = dumpbin_db.find().skip(dumpbin_db.count() - 1)
     res = next(output_cur, '0')
+    res.update({"_id": str(res.get("_id"))})
     return jsonify({'result': res})
 
 
 @application.route('/bins-all', methods=['GET'])
-def get_veh_dist_all():
+def get_bins_all():
     output = []
     for s in dumpbin_db.find():
+        s.update({"_id": str(s.get("_id"))})
         output.append(s)
     return jsonify({'result': output})
 
 
 @application.route('/set-bin-status', methods=['POST'])
-def set_bin_status():
+def insert_bin_status():
     bin_id = request.json['bin_id']
     ctime = request.json['datetime']
     status = request.json['status']
@@ -48,7 +50,7 @@ def set_bin_status():
     upd_id = dumpbin_db.insert_one(
         {'_id': "{}-{}".format(bin_id, ctime), 'bin_id': bin_id, 'status': status, 'geolocation': geolocation,
          'region': region})
-    return ({'result': upd_id.inserted_id if upd_id.acknowledged else False})
+    return jsonify({'result': upd_id.inserted_id if upd_id.acknowledged else False})
 
 
 @application.route('/bins-alert', methods=['GET'])
@@ -58,24 +60,46 @@ def get_all_bins_alert():
     count_row = (dumpbin_db.count() - 1) if dumpbin_db.count() > 0 else False
     if not count_row:
         print("No Dumpbins status updated and tracked")
-        return  ({'result': None})
-    elif not dumpbin_db.find({'region': region, 'status': status}).count():
+        return jsonify({'result': "No Dumpbins status updated and tracked"})
+    elif (status and region)and not dumpbin_db.find({'region': region, 'status': status}).count():
         print("For region {} No Dumpbins is {}".format(region,status))
-        return ({'result': None})
-    else:
+        return jsonify({'result': "For region {} No Dumpbins is {}".format(region,status)})
+    elif status and region:
         result_cur = dumpbin_db.find({'region': region, 'status': status})
-        return ({'result': list(result_cur) if result_cur else None})
+        result=[]
+        if result_cur:
+            for row in result_cur:
+                row.update({"_id": str(row.get("_id"))})
+                result.append(row)
+        return jsonify({'result': result if result else None})
+    elif status and not region:
+        result_cur = dumpbin_db.find({'status': status})
+        result = []
+        if result_cur:
+            for row in result_cur:
+                row.update({"_id": str(row.get("_id"))})
+                result.append(row)
+        return jsonify({'result': result if result else None})
+    elif not status and region:
+        result_cur = dumpbin_db.find({'region': region})
+        result = []
+        if result_cur:
+            for row in result_cur:
+                row.update({"_id": str(row.get("_id"))})
+                result.append(row)
+        return jsonify({'result': result if result else None})
+    else:
+        print("Please pass valid region or status")
+        return jsonify({'result': "Please pass valid region or status"})
 
-def update_bin_status(request):
-    bin_id = request.json['bin_id']
-    ctime = request.json['datetime']
-    status = request.json['status']
+@application.route('/reset-bin-status', methods=['PUT'])
+def update_bin_status():
+    bin_id = str(request.json['bin_id'])
     region = request.json['region']
-    geolocation = request.json['gelocation']
-    upd_id = dumpbin_db.insert_one(
-        {'_id': "{}-{}".format(bin_id, ctime), 'bin_id': bin_id, 'status': status, 'geolocation': geolocation,
-         'region': region})
-    return jsonify({'result': upd_id})
+    upd_id = dumpbin_db.update_one({'bin_id': bin_id,'region' : region}, {'$set': {'status': 'empty'}})
+    return jsonify({'result': {'modified_count': upd_id.modified_count,
+                               'matched_count': upd_id.matched_count} if upd_id.acknowledged else False})
+
 
 if __name__ == '__main__':
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
